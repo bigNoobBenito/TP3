8079ac90a18957e5d0a3a806b236d1e08740c399
yehouenoubk <kyehouenou@gmail.com>
2022-09-30 09:15:47 -0400

    debut du TP3

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello')
+import random
+player_life = 20
+def fight():
+    global player_life, difficulty
+    difficulty = random.randint(1,6)
+    player_strengh = random.randint(1,10)
+    if difficulty > player_strengh:
+        player_life -= difficulty
+        print(" vous avez gagne ")
+    else:
+        player_life += difficulty
+        print(" vous avez perdu ")
+
+def go_away():
+    player_life -= 1
+
